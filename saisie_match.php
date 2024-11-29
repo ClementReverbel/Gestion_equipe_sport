bdc874367a0a5360c8251ca129f2ffb80c95ff45
Salairedesalledebain <anonymat2789@gmail.com>
2024-11-29 10:58:35 +0100

    Création formulaire ajout joueurs

---
+++ saisie_match.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+        <meta charset="utf-8">
+        <title>Gestion volley : Saisie feuille de match</title>
+        <link href="style/style_connexion.css" rel="stylesheet">
+    </head>
+    <body>
+        <h2>Ajout des joueurs pour le match </h2>
+    </body>
+    </body>
+</html>
