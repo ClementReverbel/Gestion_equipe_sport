bd820dd7bf63a761b07fcd24d99a20d5aa38835e
Salairedesalledebain <anonymat2789@gmail.com>
2025-01-10 11:00:19 +0100

    indentation et commentaires de Gestions joueurs

--- pages/Gestion_joueurs_matchs.php
+++ pages/Gestion_joueurs_matchs.php
@@ -9,6 +9,7 @@
         <link href="style/style_connexion.css" rel="stylesheet">
     </head>
     <body>
+        <!-- formulaire de création d'un joueur -->
         <div id ="ajout_joueur">
             <h3>Ajouter un joueur</h2>
             <form method="POST" action="">
@@ -54,10 +55,22 @@
         </div>
         <?php
         try{
-            if(isset($_POST['submdp']) && isset($_POST['NumLic']) && isset($_POST['nomJ']) && isset($_POST['prenomJ']) && isset($_POST['dateJ']) && isset($_POST['tailleJ']) && isset($_POST['poid']) && isset($_POST['commentaire']) && isset($_POST['statut'])){
-            $linkpdo = new PDO("mysql:host=localhost;dbname=volleytrack_bd","root","");
-            $requete = $linkpdo->prepare('INSERT INTO joueurs(Numéro_de_licence,Nom,Prenom,Date_de_naissance ,Taille,Poids,Commentaire,Statut) VALUES (:num,:Nom,:Prenom,:Date_de_naissance ,:Taille, :Poids,:Commentaire,:Statut)');
-            $requete->execute(array('num'=>$_POST['NumLic'],'Nom'=>$_POST['nomJ'],'Prenom'=>$_POST['prenomJ'],'Date_de_naissance'=>$_POST['dateJ'],'Taille'=>$_POST['tailleJ'],'Poids'=>$_POST['poid'],'Commentaire'=>$_POST['commentaire'],'Statut'=>$_POST['statut']));
+            //si le bouton à d'envoi à été préssé et que le formulaire est remplit
+            if(isset($_POST['submdp']) && 
+            isset($_POST['NumLic']) && isset($_POST['nomJ']) && 
+            isset($_POST['prenomJ']) && isset($_POST['dateJ']) && 
+            isset($_POST['tailleJ']) && isset($_POST['poid']) && 
+            isset($_POST['commentaire']) && isset($_POST['statut'])){
+                //Connexion à la bs
+                $linkpdo = new PDO("mysql:host=localhost;dbname=volleytrack_bd","root","");
+                //Insertion du nouveau joueur
+                $requete = $linkpdo->prepare('INSERT INTO joueurs(Numéro_de_licence,Nom,Prenom,Date_de_naissance ,Taille,Poids,Commentaire,Statut)
+                VALUES (:num,:Nom,:Prenom,:Date_de_naissance ,:Taille, :Poids,:Commentaire,:Statut)');
+                //liaison du formulaire à la requete SQL
+                $requete->execute(array('num'=>$_POST['NumLic'],'Nom'=>$_POST['nomJ'],
+                'Prenom'=>$_POST['prenomJ'],'Date_de_naissance'=>$_POST['dateJ'],
+                'Taille'=>$_POST['tailleJ'],'Poids'=>$_POST['poid'],
+                'Commentaire'=>$_POST['commentaire'],'Statut'=>$_POST['statut']));
             }
         }
         catch(Exception $e){
@@ -67,6 +80,7 @@
 
         <div id="recherche_joueur">
         <h3>Rechercher un joueur</h3>
+        <!--Section de recherche du joueur à modifier ou supprimer -->
         <form method="POST" action="">
             <label>Numéro de licence : </label>
             <input type="text" name="rechNumLic" required>
@@ -76,16 +90,20 @@
 
     <?php
     try {
+        //Connexion à la bd
         $linkpdo = new PDO("mysql:host=localhost;dbname=volleytrack_bd", "root", "");
 
+        //Si l'utilisateur veux recherher
         if (isset($_POST['rech']) && !empty($_POST['rechNumLic'])) {
             $rechNumLic = $_POST['rechNumLic'];
+            //On recherche le joueur grace au numéro de licence indiqué
             $requete = $linkpdo->prepare('SELECT * FROM joueurs WHERE Numéro_de_licence = :num');
             $requete->execute(['num' => $rechNumLic]);
             $joueur = $requete->fetch(PDO::FETCH_ASSOC);
 
             if ($joueur) {
                 ?>
+                <!-- Section du formulaire préremplit avec les info du joueur trouvé -->
                 <div id="modifier_joueur">
                     <h3>Modifier un joueur</h3>
                     <form method="POST" action="">
@@ -123,18 +141,25 @@
                                 <option value="Absent">Absent</option>
                             </select>
                         </li>
+                        <!-- 2 cas possible: soit il veux modifier le joueur, soit il veux le supprimer -->
                         <input type="submit" name="update" value="Mettre à jour">
                         <input type="submit" name="supprimer" value="Supprimer">
                     </form>
                 </div>
                 <?php
             } else {
+                //Si le joueur n'est pas trouvé, on affiche une erreur
                 echo "<p>Joueur introuvable avec ce numéro de licence.</p>";
             }
         }
-
+        //Si l'utilisateur à choisit de modifier le joueur
         if (isset($_POST['update'])) {
-            $updateRequete = $linkpdo->prepare('UPDATE joueurs SET Nom = :Nom, Prenom = :Prenom, Date_de_naissance = :Date_de_naissance, Taille = :Taille, Poids = :Poids, Commentaire = :Commentaire, Statut = :statut WHERE Numéro_de_licence = :num');
+            //Création de la requete SQL
+            $updateRequete = $linkpdo->prepare('UPDATE joueurs SET Nom = :Nom, Prenom = :Prenom,
+            Date_de_naissance = :Date_de_naissance, Taille = :Taille, 
+            Poids = :Poids, Commentaire = :Commentaire, Statut = :statut 
+            WHERE Numéro_de_licence = :num');
+            //liaison du formulaire à la requete SQL
             $updateRequete->execute([
                 'Nom' => $_POST['nomJ'],
                 'Prenom' => $_POST['prenomJ'],
@@ -148,15 +173,18 @@
 
             echo "<p>Joueur mis à jour avec succès !</p>";
         }
-
+        //Si l'utilisateur à choisit de supprimer le joueur
         if (isset($_POST['supprimer'])) {
+            //Création de la requete pour avoir le nombre de match auquel à participer le joueur
             $nbMatchRequete = $linkpdo->prepare('SELECT count(*) FROM participer p  WHERE p.idJoueur = (SELECT idJoueur FROM joueurs WHERE Numéro_de_licence = :num)');
             $nbMatchRequete->execute([
                 'num' => $_POST['NumLic']
             ]);
             $nombreDeMatchs = $nbMatchRequete->fetchColumn();
+            //Si le joueur à participer à un match, on ne peu pas le supprimer
             if ($nombreDeMatchs >= 1){
                 echo "<p>Impossible de supprimer le joueur car il a déjà participer à un match</p>";
+            //Sinon on le supprime
             } else {
             $supprimerRequete = $linkpdo->prepare('DELETE FROM joueurs WHERE Numéro_de_licence = :num');
             $supprimerRequete->execute([
